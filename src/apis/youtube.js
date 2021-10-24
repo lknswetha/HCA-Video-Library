cb315068dfc3f18fd2600e3f84f46c0cec6d0660
lknswetha <92875027+lknswetha@users.noreply.github.com>
2021-10-24 11:55:48 -0400

    Updates added for youtube file.

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const KEY = 'AIzaSyCczSGIr2Mw7xlBu7R41cMAnEawOy_W3pw';
+const KEY = 'AIzaSyC8Vcb6mScRmUQPMCJ70cI2pma-0Cq4apo';
 const CHANNEL_ID ='UCL03ygcTgIbe36o2Z7sReuQ';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
         part: 'snippet',
-        maxResults: 5,
+        maxResults: 10,
         key: KEY,
         channelId:CHANNEL_ID
     }
