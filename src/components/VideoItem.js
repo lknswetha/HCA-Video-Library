7d119d33ed99b779552c423a297a808057f96b9f
lknswetha <92875027+lknswetha@users.noreply.github.com>
2021-10-24 15:49:13 -0400

    Updated description on video item.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,12 +3,11 @@ import moment from 'moment';
 import '../style/video.css';
 
 const VideoItem = ({video , handleVideoSelect,handleVideoOnLoad}) => {
-    debugger;
     return (
         <div onClick={ () => handleVideoSelect(video)}  onLoad={() => handleVideoOnLoad(video)} className=' video-item item'>
             <img className='ui image' src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
             <div className='content'>
-                <div className='header '>{video.snippet.title} <br/> published date :  { moment().format('MMM DD h:mm A',video.snippet.publishTime)}</div>
+                <div className='header '>{video.snippet.title} <br/> Description: { video.snippet.description } <br/> published date :  { moment().format('MMM DD h:mm A',video.snippet.publishTime)}</div>
             </div>
         </div>
     )
