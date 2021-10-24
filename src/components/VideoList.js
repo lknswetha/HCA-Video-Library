527e3a9ab3730e13dbed6b62fcbe68dbbb858cdf
lknswetha <92875027+lknswetha@users.noreply.github.com>
2021-10-24 15:58:49 -0400

    Removed debugger.

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,6 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({videos , handleVideoSelect,handleVideoOnLoad}) => {
     //handleVideoOnLoad={this.handleVideoOnLoad} 
-    debugger;
     const renderedVideos =  videos.map((video) => {
         
         return <VideoItem key={video.id.videoId} video={video} handleVideoSelect={handleVideoSelect} handleVideoOnLoad ={handleVideoOnLoad} />
